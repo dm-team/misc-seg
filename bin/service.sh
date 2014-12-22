8cc80186ea7883953c5028dc89371f8d2ef1978a
xh <xionghan00@126.com>
2014-12-22 11:45:54 +0800

    fix shell start script

--- bin/service.sh
+++ bin/service.sh
@@ -22,6 +22,7 @@ if [ -x "$JAVA_HOME/bin/java" ]; then
     JAVA="$JAVA_HOME/bin/java"
 else
     JAVA=`which java`
+fi
 
 if [ ! -x "$JAVA" ]; then
     echo "Could not find any executable java binary. Please install java in your PATH or set JAVA_HOME"
